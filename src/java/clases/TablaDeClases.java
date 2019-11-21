acf0a598c2190c9d92d064513a242fd4799c9139
enrique <enrique@localhost>
2019-11-21 12:58:35 +0100

    Kike 8

---
+++ src/java/clases/TablaDeClases.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author enrique
+ */
+public class TablaDeClases {
+
+    private String clase = null;
+    private String horario = null;
+    private String monitor = null;
+    private String id_clase = null;
+
+    public TablaDeClases() {
+    }
+
+    /**
+     * @return the clase
+     */
+    public String getClase() {
+        return clase;
+    }
+
+    /**
+     * @param clase the clase to set
+     */
+    public void setClase(String clase) {
+        this.clase = clase;
+    }
+
+    /**
+     * @return the horario
+     */
+    public String getHorario() {
+        return horario;
+    }
+
+    /**
+     * @param horario the horario to set
+     */
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    /**
+     * @return the monitor
+     */
+    public String getMonitor() {
+        return monitor;
+    }
+
+    /**
+     * @param monitor the monitor to set
+     */
+    public void setMonitor(String monitor) {
+        this.monitor = monitor;
+    }
+
+    /**
+     * @return the id_clase
+     */
+    public String getId_clase() {
+        return id_clase;
+    }
+
+    /**
+     * @param id_clase the id_clase to set
+     */
+    public void setId_clase(String id_clase) {
+        this.id_clase = id_clase;
+    }
+
+}
---
+++ src/java/clases/mostrarClasesDelHorario.java
@@ -0,0 +1,201 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author enrique
+ */
+public class mostrarClasesDelHorario extends HttpServlet {
+
+    DataSource datasource;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    public void init() throws ServletException {
+
+        try {
+            InitialContext initialContext = new InitialContext();
+            datasource = (DataSource) initialContext.lookup("jdbc/CEUFIT01");
+            Connection connection = datasource.getConnection();
+            Statement createStatement = connection.createStatement();
+        } catch (NamingException | SQLException ex) {
+            Logger.getLogger(mostrarInformacion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+//    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        response.setContentType("text/html;charset=UTF-8");
+//        try (PrintWriter out = response.getWriter()) {
+//            /* TODO output your page here. You may use following sample code. */
+//            out.println("<!DOCTYPE html>");
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Servlet mostrarClasesDelHorario</title>");            
+//            out.println("</head>");
+//            out.println("<body>");
+//            out.println("<h1>Servlet mostrarClasesDelHorario at " + request.getContextPath() + "</h1>");
+//            out.println("</body>");
+//            out.println("</html>");
+//        }
+//    }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+//        processRequest(request, response);
+        ServletContext contexto = request.getServletContext();
+        String query = "SELECT CLASE, HORARIO, MONITOR, ID_CLASE FROM CLASES;";
+        ResultSet resultSet = null;
+        Statement statement = null;
+        Connection connection = null;
+        TablaDeClases clases = new TablaDeClases();
+        try {
+            connection = datasource.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(query);
+            ArrayList arrayClases = new ArrayList();
+
+            while (resultSet.next()) {
+                clases.setClase(resultSet.getString("CLASE"));
+                clases.setHorario(resultSet.getString("HORARIO"));
+                clases.setMonitor(resultSet.getString("MONITOR"));
+                clases.setId_clase(resultSet.getString("ID_CLASE"));
+                arrayClases.add(clases);
+            }
+
+            request.setAttribute("TablaDeClases", arrayClases);
+            RequestDispatcher rd = contexto.getRequestDispatcher("/apuntarseClase.xhtml");
+            rd.forward(request, response);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(mostrarInformacion.class.getName()).log(Level.SEVERE,
+                    "Falló la consulta", ex);
+        } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(mostrarInformacion.class.getName()).log(Level.SEVERE,
+                            "No se pudo cerrar el Resulset", ex);
+                }
+            }
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(mostrarInformacion.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(mostrarInformacion.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs EN EL MÉTODO DOPOST, VA A
+     * COGER EL ID_CLASE Y SE LO VA A ASIGNAR A UN USUARIO DE UNA TABLA
+     * ID_USUARIO-ID_CLASE. PARA SABER EL USUARIO, TIENE QUE GUARDAR LA SESIÓN Y
+     * EL ID_USUARIO SERÁ UN ATRIBUTO DE LA SESIÓN.
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+//        processRequest(request, response);
+
+        HttpSession sesion = request.getSession();
+//        String id_usuario = (String) sesion.getAttribute("id_usuario");
+        ServletContext contexto = request.getServletContext();
+        String query = "INSERT INTO APUNTADOS(ID_USUARIO, ID_CLASE) VALUE('" + sesion.getAttribute("id_usuario")
+                + "'," + request.getAttribute("id_clase") + "');";
+        Statement statement = null;
+        Connection connection = null;
+        try {
+            connection = datasource.getConnection();
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(mostrarInformacion.class.getName()).log(Level.SEVERE,
+                    "Falló la consulta", ex);
+        } finally {
+
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(mostrarInformacion.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(mostrarInformacion.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
