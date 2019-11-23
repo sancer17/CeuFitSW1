3d319761882bbbeef73a2bcfd17365258a5f8ad6
alexgv95 <alejandrogutierrezyermo@yahoo.es>
2019-11-23 17:26:57 +0100

    Funcionalidad clases de vista Admin al 60%

---
+++ src/java/clases/Clase.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author Alejandro
+ */
+
+public class Clase {
+    String clase;
+    String descripcion;
+    String horario;
+    String monitor;
+
+    public Clase(String clase, String descripcion, String horario, String monitor) {
+        this.clase = clase;
+        this.descripcion = descripcion;
+        this.horario = horario;
+        this.monitor = monitor;
+    }
+
+    public String getClase() {
+        return clase;
+    }
+
+    public void setClase(String clase) {
+        this.clase = clase;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public String getMonitor() {
+        return monitor;
+    }
+
+    public void setMonitor(String monitor) {
+        this.monitor = monitor;
+    }
+
+    @Override
+    public String toString() {
+        return "Clase{" + "clase=" + clase + ", descripcion=" + descripcion + ", horario=" + horario + ", monitor=" + monitor + '}';
+    }
+}
---
+++ src/java/clases/ClasesBean.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author Alejandro
+ */
+
+@Named
+@SessionScoped
+public class ClasesBean implements Serializable {
+
+    private final ArrayList<Clase> clases;
+
+    public ClasesBean() {
+        System.out.println("Cargando clases ...");
+        this.clases = new ArrayList<>();
+
+        try {
+            InitialContext initialContext = new InitialContext();
+            DataSource datasource = (DataSource) initialContext.lookup("jdbc/CEUFIT01");
+            String query = null;
+            query = "SELECT *" + "FROM CLASES";
+
+            Connection connection = datasource.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                Clase clase = new Clase(resultSet.getString("clase"), resultSet.getString("descripcion"),
+                        resultSet.getString("horario"), resultSet.getString("monitor"));
+                clases.add(clase);
+                System.out.println("Clase: " + clase);
+            }
+        } catch (SQLException | NamingException ex) {
+            Logger.getLogger(ClasesBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public ArrayList<Clase> getClases() {
+        return clases;
+    }
+}
